f18d97b2f2fa29500d636bc9624bc87e4acb5917
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-09-22 12:13:12 -0300

    Add method inside definition

--- script.rb
+++ script.rb
@@ -6,10 +6,13 @@ end
 
 
 def bubbleSort(arr)
-    if item[index] > item[index + 1]
-        el = x.slice!(0)
-        x.insert(1, el)
+  arr.each_with_index do |item, index|
+    p "At #{index} we have #{item}"
+    if arr[index] > arr[index + 1]
+      el = x.slice!(0)
+      x.insert(1, el)
     end
+  end
 end
 
 p bubbleSort(arr)
