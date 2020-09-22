a4634b07cfa09be8dbceb3dec30231e3323833ed
Gabriel Suárez <ginnlaiho@gmail.com>
2020-09-22 10:50:05 -0500

    Algorithm Complete
    Completed the algorithm for "bubble sort" sorting method

--- script.rb
+++ script.rb
@@ -1,18 +1,25 @@
 arr = [5, 4, 3, 2, 1, 0]
 
-arr.each_with_index do |item, index|
-  p "At #{index} we have #{item}"
-end
-
-
 def bubbleSort(arr)
-  arr.each_with_index do |item, index|
-    p "At #{index} we have #{item}"
-    if arr[index] > arr[index + 1]
-      el = x.slice!(0)
-      x.insert(1, el)
+  (arr.length - 1).times do arr.each_with_index do |item, index|
+    if index == arr.length - 1
+      next
+      elsif item > arr[index + 1]
+        el = arr.slice!(index)
+        arr.insert(index + 1, el)
+        end
     end
   end
+  return arr
 end
 
 p bubbleSort(arr)
+
+
+
+
+
+ 
+
+
+
