3983cb163aa7b4d8d43d51e417de90eb0535f42f
Gabriel Suárez <ginnlaiho@gmail.com>
2020-09-22 10:08:13 -0500

    Pseudo Code for Bubble Sort
    Building the method

--- script.rb
+++ script.rb
@@ -1,5 +1,15 @@
-arr = [2, 3, 4]
+arr = [5, 4, 3, 2, 1, 0]
 
 arr.each_with_index do |item, index|
   p "At #{index} we have #{item}"
 end
+
+
+def bubbleSort(arr)
+    if item[index] > item[index + 1]
+        el = x.slice!(0)
+        x.insert(1, el)
+    end
+end
+
+p bubbleSort(arr)
